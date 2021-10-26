839c52cb19e7d452a3b545164437724e817959a1
andrius.kaliacius <andrius.kaliacius@smarsh.com>
2021-10-26 08:35:37 +0100

    changes in main verticle

--- src/main/java/com/akaliacius/demos/vertx_timer_service/MainVerticle.java
+++ src/main/java/com/akaliacius/demos/vertx_timer_service/MainVerticle.java
@@ -1,29 +1,49 @@
 package com.akaliacius.demos.vertx_timer_service;
 
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Promise;
-import io.vertx.core.Vertx;
+import com.akaliacius.demos.vertx_timer_service.verticles.TimerManager;
+import com.akaliacius.demos.vertx_timer_service.verticles.TimersRestApi;
+import io.vertx.core.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static com.akaliacius.demos.vertx_timer_service.utils.LoggingUtils.logDeploymentFailure;
+import static com.akaliacius.demos.vertx_timer_service.utils.LoggingUtils.logDeploymentSuccess;
 
 public class MainVerticle extends AbstractVerticle {
 
+  private static final Logger logger = LoggerFactory.getLogger(MainVerticle.class);
+
   public static void main(String[] args) {
     var vertx = Vertx.vertx();
-    vertx.deployVerticle(new MainVerticle());
+    vertx.exceptionHandler(error -> logger.error("Unhandled: {}", error));
+    vertx.deployVerticle(new MainVerticle())
+      .onFailure(error -> logDeploymentFailure(error, MainVerticle.class, logger))
+      .onSuccess(id -> logDeploymentSuccess(id, MainVerticle.class, logger));
   }
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(8888, http -> {
-      if (http.succeeded()) {
+    vertx.deployVerticle(new TimerManager())
+      .onSuccess(id -> logDeploymentSuccess(id, TimerManager.class, logger))
+      .onFailure(error -> {
+        logDeploymentFailure(error, TimerManager.class, logger);
+        startPromise.fail(error);
+      })
+      .compose(next -> deployRestApiVerticle(startPromise));
+  }
+
+  private Future<String> deployRestApiVerticle(Promise<Void> startPromise){
+    return vertx.deployVerticle(
+        TimersRestApi.class.getName(),
+        new DeploymentOptions().setInstances(2)
+      )
+      .onFailure(error -> {
+        logDeploymentFailure(error, TimersRestApi.class, logger);
+        startPromise.fail(error);
+      })
+      .onSuccess(id -> {
+        logDeploymentSuccess(id, TimersRestApi.class, logger);
         startPromise.complete();
-        System.out.println("HTTP server started on port 8888");
-      } else {
-        startPromise.fail(http.cause());
-      }
-    });
+      });
   }
 }
